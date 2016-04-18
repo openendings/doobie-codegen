4d7939af7d147adef9fe81de470fb2d0b67fcb78
Matthew Moss <matthew@premonition.io>
2016-04-18 13:44:56 +1000

    Pass config through and seperate main

--- src/main/scala/mdmoss/doobiegen/Runner.scala
+++ src/main/scala/mdmoss/doobiegen/Runner.scala
@@ -11,7 +11,17 @@ import scala.util.{Failure, Success}
 
 object Runner {
 
-  val config = Target(
+  case class TestDatabase(driver: String, url: String, username: String, password: String)
+  case class Target(
+    schemaDir: String,
+    testDb: TestDatabase,
+    `package`: String) {
+
+    def enclosingPackage = `package`.split('.').reverse.headOption
+  }
+
+  val Default = Target(
+    schemaDir = "sql/",
     TestDatabase(
       "org.postgresql.Driver",
       "jdbc:postgresql:gen",
@@ -21,13 +31,12 @@ object Runner {
     `package` = "mdmoss.doobiegen.db"
   )
 
-  def main(args: Array[String]) {
-
+  def run(target: Target) = {
     /* We could do something more intelligent here, but this works for now */
     val sql = {
       import scala.sys.process._
       val buffer = new ListBuffer[String]()
-      val proc = ("bash" :: "-c" :: "cat sql/*.sql" :: Nil).run(ProcessLogger(s => buffer.append(s)))
+      val proc = ("bash" :: "-c" :: s"cat ${target.schemaDir}*.sql" :: Nil).run(ProcessLogger(s => buffer.append(s)))
       proc.exitValue()
       buffer.mkString("\n")
     }
@@ -42,7 +51,7 @@ object Runner {
     val parsers = cleanedAndSplit.map(new SqlStatementParser(_))
 
     parsers.foreach { s =>
-      println(seperator)
+      println(Seperator)
       println(s.input.sliceString(0, s.input.length))
       s.StatementLine.run() match {
         case r@Success(_) => println(r)
@@ -54,7 +63,7 @@ object Runner {
 
     val statements = parsers.flatMap(_.StatementLine.run().toOption)
 
-    println(seperator)
+    println(Seperator)
 
     if (statements.length != parsers.length) throw new Throwable("Failed parsing. Exiting.")
 
@@ -63,7 +72,7 @@ object Runner {
     /*    model.tables.foreach(println)
     println(seperator)*/
 
-    val analysis = new Analysis(model, config)
+    val analysis = new Analysis(model, Default)
 
     model.tables.foreach { t =>
       /*      println(seperator)
@@ -84,12 +93,12 @@ object Runner {
 
     SourceWriter.write(Paths.get("out/src"), files)
 
+
   }
 
-  val seperator = "*" * 80
-
-  case class TestDatabase(driver: String, url: String, username: String, password: String)
-  case class Target(testDb: TestDatabase, `package`: String) {
-    def enclosingPackage = `package`.split('.').reverse.headOption
+  def main(args: Array[String]) = {
+    run(Default)
   }
+
+  val Seperator = "*" * 80
 }
