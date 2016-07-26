fc9f444df630e874cf3ddc80e8e90c18c51c0d7b
Matthew Moss <mdmoss@mdmoss.net>
2016-07-26 15:56:53 +1000

    Play with unnest and ordinality for ordering

--- build.sbt
+++ build.sbt
@@ -24,3 +24,16 @@ lazy val out = (project in file("out"))
       "org.specs2"    %% "specs2-core"               % "3.6.3" % "test"
     )
   )
+
+lazy val test = (project in file("test"))
+  .settings(commonSettings:_*)
+  .settings(
+    resolvers += "tpolecat" at "http://dl.bintray.com/tpolecat/maven",
+    libraryDependencies ++= Seq(
+      "io.argonaut"   %% "argonaut"                  % "6.1",
+      "org.tpolecat"  %% "doobie-core"               % "0.2.3",
+      "org.tpolecat"  %% "doobie-contrib-postgresql" % "0.2.3",
+      "org.tpolecat"  %% "doobie-contrib-specs2"     % "0.2.3" % "test",
+      "org.specs2"    %% "specs2-core"               % "3.6.3" % "test"
+    )
+  )
---
+++ test/src/test/scala/mdmoss/doobiegen/queries/UnnestOrdinalitySpec.scala
@@ -0,0 +1,63 @@
+package mdmoss.doobiegen.queries
+
+import doobie.imports._
+import org.specs2.mutable.Specification
+import doobie.contrib.postgresql.pgtypes._
+import org.specs2.matcher.ThrownExpectations
+
+import scalaz.concurrent.Task
+
+object UnnestOrdinalitySpec extends Specification with ThrownExpectations {
+
+  val xa = DriverManagerTransactor[Task]("org.postgresql.Driver", "jdbc:postgresql:gen", "test", "test")
+
+  sequential
+
+  def unnestOrdinalityQ(as: Option[List[Int]], bs: Option[List[String]]) = {
+    sql"""SELECT unnest_ordinality.a, unnest_ordinality.b
+          FROM unnest_ordinality
+          LEFT JOIN unnest(${{as}.toSeq.flatten.toArray}::int[]) WITH ORDINALITY t1(val, ord) ON t1.val = unnest_ordinality.a
+          LEFT JOIN unnest(${{bs}.toSeq.flatten.toArray}::text[]) WITH ORDINALITY t2(val, ord) ON t2.val = unnest_ordinality.b
+
+          WHERE (${as.isEmpty} OR unnest_ordinality.a = ANY(${{as}.toSeq.flatten.toArray}))
+            AND (${bs.isEmpty} OR unnest_ordinality.b = ANY(${{bs}.toSeq.flatten.toArray}))
+
+          ORDER BY t1.ord, t2.ord
+          """.query[(Int, String)]
+  }
+
+  "unnest-ordinality based queries should" >> {
+
+    xa.trans(sql"""CREATE TABLE IF NOT EXISTS unnest_ordinality (a INT NOT NULL, b TEXT NOT NULL);""".update.run).run
+    xa.trans(sql"""DELETE FROM unnest_ordinality""".update.run).run
+
+    "return nothing as expected from an empty table" >> {
+      xa.trans(unnestOrdinalityQ(None, None).list).run must_== List()
+    }
+
+    "select everything when there's no filters applied" >> {
+      xa.trans(sql"""INSERT INTO unnest_ordinality VALUES (1, 'a'), (2, 'b'), (3, 'a')""".update.run).run
+      xa.trans(unnestOrdinalityQ(None, None).list).run.toSet must_== Set((1, "a"), (2, "b"), (3, "a"))
+    }
+
+    "filter properly when there's only one filter" >> {
+      xa.trans(unnestOrdinalityQ(Some(List(1)), None).list).run must_== List((1, "a"))
+    }
+
+    "filter properly cross-filters" >> {
+      xa.trans(unnestOrdinalityQ(Some(List(1)), Some(List("c"))).list).run must_== List()
+    }
+
+    "return results in the expected order" >> {
+      xa.trans(unnestOrdinalityQ(Some(List(1, 2)), None).list).run must_== List((1, "a"), (2, "b"))
+      xa.trans(unnestOrdinalityQ(Some(List(2, 1)), None).list).run must_== List((2, "b"), (1, "a"))
+    }
+
+    "filter multiple results properly" >> {
+      xa.trans(unnestOrdinalityQ(Some(List(1)), Some(List("a"))).list).run must_== List((1, "a"))
+    }
+
+
+  }
+
+}
